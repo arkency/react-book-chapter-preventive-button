371bb2ded073a0495d6ce8e80c66860fb1fd0e74
Fernando <krajka.bartosz@gmail.com>
2015-08-18 17:00:27 +0200

    Added two function as an internal interface

--- app/components/PreventiveButton.js
+++ app/components/PreventiveButton.js
@@ -5,6 +5,17 @@ class PreventiveButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { disabled: false };
+
+    this.enable = this.enable.bind(this);
+    this.disable = this.disable.bind(this);
+  }
+
+  enable() {
+    this.setState( { disabled: false } );
+  }
+
+  disable() {
+    this.setState( { disabled: true } );
   }
 
   render () {
